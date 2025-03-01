f21d40c169a38393bb94ed7fae2cb442e265d9c4
kericsaba <keri.csaba@student.ms.sapientia.ro>
2025-03-01 08:25:05 +0200

    Frontend: Fix RootNavigation

--- Frontend/src/navigation/RootNavigation.tsx
+++ Frontend/src/navigation/RootNavigation.tsx
@@ -3,7 +3,7 @@ import { AuthStackNavigation } from "../navigation/StackNavigation";
 import { getData } from "../utils";
 
 export const RootNavigation = () => {
-  if (getData("token") !== null) {
+  if (getData('AUTH_TOKEN') !== null) {
     return <AuthStackNavigation />;
   } else return <RootTabNavigation />;
 };
